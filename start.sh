4bbd77477528b5be08bc7dd12a4aa9ae280d4131
sonnydg <sonny.dg@gmail.com>
2017-04-10 16:32:10 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 #./steamcmd.sh +login anonymous +force_install_dir /server/csgo +app_update 740 validate +quit      
 
-cd $HOME/hlserver
+cd $SERVER
 #csgo/srcds_run -game csgo -console -usercon +game_type $GAME_TYPE \
 #                               +game_mode $GAME_MODE \
 #                               +mapgroup $MAPGROUP \
