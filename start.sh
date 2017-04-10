da822a413a31bab2dfe505f3ac5bf154d944a147
sonnydg <sonny.dg@gmail.com>
2017-04-10 16:46:49 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd $HOME/hlserver/csgo
+cd /home/root/hlserver/csgo
 ./srcds_run -game csgo -tickrate 128 -autoupdate -steam_dir ~/hlserver -steamcmd_script ~/hlserver/csgo_ds.txt $@
 
 #./steamcmd.sh +login anonymous +force_install_dir /server/csgo +app_update 740 validate +quit      
